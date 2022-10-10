640e1c92318156757fadc4df5a80a2f4fa01c66d
Shawaal-Nadeem <hafizshawalnadeem@gmail.com>
2022-10-10 12:02:54 -0700

    Working on Responsiveness

--- src/components/home.js
+++ src/components/home.js
@@ -1,27 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 function Home(){
+  const [toggleMenu, setToggleMenu] = useState(false)
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu)
+  }
+
+  useEffect(() => {
+
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', changeWidth)
+
+    return () => {
+        window.removeEventListener('resize', changeWidth)
+    }
+
+  }, [])
+
 return(
 <div>
 
 <div className="hero">
+
   <nav>
-    <h2 class="logo">Portfo<span>lio</span></h2>
-    <ul>
-    <li><Link exact to="/">Home</Link></li>
-      <li><Link to="/about">About Us</Link></li>
-      <li><Link to="/services">Services</Link></li>
-      <li><Link to="/skills">Skills</Link></li>
-      <li><Link to="/contact-us">Contact Us</Link></li>
-    </ul>
-    <button class="btn">Subscribe</button>
+  <div onClick={toggleNav} className="menu-button">
+<div className="bar"></div>
+<div className="bar"></div>
+<div className="bar"></div>
+</div>   
+   
   </nav>
+          <nav>
+{(toggleMenu || screenWidth>869 ) && (
+      <ul className="list">
+    <li className="items"><Link exact to="/">Home</Link></li>
+      <li className="items"><Link to="/about">About Us</Link></li>
+      <li className="items"><Link to="/services">Services</Link></li>
+      <li className="items"><Link to="/skills">Skills</Link></li>
+      <li className="items"><Link to="/contact-us">Contact Us</Link></li>
+    </ul>
+    )}
+    </nav>
+    <h2 class="logo">Portfo<span>lio</span></h2>
 
   <div class="content">
+    
     <h4>Hello, my name is</h4>
     <h1>Shawaal <span>Nadeem</span></h1>
     <h3>I'am a Web Developer.</h3>
+    
     <div class="newslatter">
+    
       <form>
         <input type="email" name="email" id="mail" placeholder="Enter Your Email"/>
         <input type="submit" name="submit" value="Lets Start"/>
--- src/components/service.js
+++ src/components/service.js
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 function Service(){
+  const [toggleMenu, setToggleMenu] = useState(false)
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu)
+  }
+
+  useEffect(() => {
+
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', changeWidth)
+
+    return () => {
+        window.removeEventListener('resize', changeWidth)
+    }
+
+  }, [])
 return(
 <div>
 <div class="hero-about">
-        <nav>
-            <h2 class="logo">Portfo<span>lio</span></h2>
-            <ul>
-              <li><Link exact to="/">Home</Link></li>
-              <li><Link to="/about">About Us</Link></li>
-              <li><Link to="/services">Services</Link></li>
-              <li><Link to="/skills">Skills</Link></li>
-              <li><Link to="/contact-us">Contact Us</Link></li>
-            </ul>
-            <button class="btn">Subscribe</button>
-          </nav>
+<nav>
+  <div onClick={toggleNav} className="menu-button">
+<div className="bar"></div>
+<div className="bar"></div>
+<div className="bar"></div>
+</div>   
+   
+  </nav>
+          <nav>
+{(toggleMenu || screenWidth>869 ) && (
+      <ul className="list">
+    <li className="items"><Link exact to="/">Home</Link></li>
+      <li className="items"><Link to="/about">About Us</Link></li>
+      <li className="items"><Link to="/services">Services</Link></li>
+      <li className="items"><Link to="/skills">Skills</Link></li>
+      <li className="items"><Link to="/contact-us">Contact Us</Link></li>
+    </ul>
+    )}
+    </nav>
+    <h2 class="logo">Portfo<span>lio</span></h2>
           </div>
 <div class="service">
   <div class="title">
